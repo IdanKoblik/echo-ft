8261661e707d5e3fd270f543af85fd32fdcd4969
idank <idankob@gmail.com>
2025-07-09 11:52:17 +0300

    Initial commit

---
+++ receiver.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "filechunk.pb-c.h"
+#include <inttypes.h>
+
+#define PORT 9000
+#define BUFFER_SIZE 2048
+
+int main(void) {
+  struct sockaddr_in receiver_addr, sender_addr;
+  socklen_t addr_len = sizeof(sender_addr);
+
+  uint8_t buffer[BUFFER_SIZE];
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    fprintf(stderr, "Cannot create socket\n");
+    return -1;
+  }
+
+  memset(&receiver_addr, 0, sizeof(receiver_addr));
+  receiver_addr.sin_family = AF_INET;
+  receiver_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  receiver_addr.sin_port = htons(PORT);
+
+  if (bind(sockfd, (struct sockaddr *)&receiver_addr, sizeof(receiver_addr)) < 0) {
+    fprintf(stderr, "Cannot bind to sender\n");
+    return -1;
+  }
+
+  FILE *file = NULL;
+  printf("Receiver listening on port %d...\n", PORT);
+
+  int total = 0;
+  while (1) {
+    ssize_t recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &addr_len);
+    if (recv_len < 0) {
+      fprintf(stderr, "Cannot receive data\n");
+      break;
+    }
+
+    FileChunk *chunk = file_chunk__unpack(NULL, recv_len, buffer);
+    if (chunk == NULL) {
+      fprintf(stderr, "Failed to unpack received data\n");
+      break;
+    }
+
+    if (file == NULL) {
+        file = fopen(chunk->filename, "wb");
+      if (!file) {
+        fprintf(stderr, "Cannot open file to write: %s\n", chunk->filename);
+        file_chunk__free_unpacked(chunk, NULL);
+        continue;
+      }
+      printf("Receiving file: %s\n", chunk->filename);
+    }
+
+    size_t written = fwrite(chunk->data.data, 1, chunk->data.len, file);
+    printf("fwrite wrote %zu bytes\n", written);
+    if (written != chunk->data.len) {
+      fprintf(stderr, "Warning: fwrite wrote fewer bytes than expected!\n");
+    } else printf("Received chunk (%d bytes)\n", chunk->size);
+
+    total += chunk->data.len;
+    if (total >= chunk->total) {
+      printf("File transfer complete.\n");
+      file_chunk__free_unpacked(chunk, NULL);
+      break;
+    }
+
+    file_chunk__free_unpacked(chunk, NULL);
+  }
+
+  printf("good\n");
+  if (file)
+    fclose(file);
+
+  close(sockfd);
+  return 0;
+}
---
+++ sender.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include "filechunk.pb-c.h"
+
+#define PORT 9000
+#define CHUNK_SIZE 1024
+
+int send_chunk(int sock, struct sockaddr_in *dest, FileChunk *chunk) {
+  size_t packed_size = file_chunk__get_packed_size(chunk);
+  uint8_t *buffer = malloc(packed_size);
+  file_chunk__pack(chunk, buffer);
+  
+  ssize_t sent = sendto(sock, buffer, packed_size, 0, (struct sockaddr *)dest, sizeof(*dest));
+  if (sent < 0) {
+    fprintf(stderr, "Cannot send data to peer\n");
+    return -1;
+  }
+ 
+      printf("Sent %zd bytes: %s chunk (%d bytes of data)\n", sent, chunk->filename, chunk->size);
+  free(buffer);
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Invalid num of args\n");
+    return -1;
+  }
+
+  char *filename = argv[1];
+  FILE* file = fopen(filename, "rb");
+  if (!file) {
+    fprintf(stderr, "Cannot find file: %s\n", filename);
+    return -1;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long total_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  int sockfd, n;
+  struct sockaddr_in dest;
+
+  bzero(&dest, sizeof(dest));
+  dest.sin_addr.s_addr = inet_addr("127.0.0.1");
+  dest.sin_port = htons(PORT);
+  dest.sin_family = AF_INET;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (connect(sockfd, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
+    fprintf(stderr, "Connection failed\n");
+    fclose(file);
+    return -1;
+  }
+
+  unsigned char buffer[CHUNK_SIZE];
+  size_t bytes_read;
+
+  while ((bytes_read = fread(buffer, 1, CHUNK_SIZE, file)) > 0) {
+    FileChunk chunk = FILE_CHUNK__INIT;
+
+    chunk.data.data = malloc(bytes_read);
+    memcpy(chunk.data.data, buffer, bytes_read);
+    chunk.data.len = bytes_read;
+
+    chunk.version = "1"; // TODO
+    chunk.filename = filename;
+    chunk.size = (int)bytes_read;
+    chunk.total_size = (int)total_size;
+
+    send_chunk(sockfd, &dest, &chunk);
+    free(chunk.data.data);
+  }
+
+  fclose(file);
+  return 0;
+}
